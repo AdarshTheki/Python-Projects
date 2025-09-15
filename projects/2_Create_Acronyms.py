5de12adda927a8697b58d584d3dbdb131f71e461
AdarshTheki <adarshverma549@gmail.com>
2025-09-15 19:31:28 +0530

    deleted

--- projects/2_Create_Acronyms.py
+++
@@ -1,8 +0,0 @@
-# Create Acronyms Using Python:-
-
-user = str(input("Enter a Phrase: "))
-text = user.split()
-a = " "
-for i in text:
-    a = a + str(i[0]).upper()
-print(a)
--- projects/9_Print_color_text.py
+++
@@ -1,9 +0,0 @@
-# Print Colored Text with Python
-
-import colorama
-from colorama import Fore, Back, Style
-colorama.init(autoreset=True)
-
-print(Fore.BLUE+Back.YELLOW+"Hi My name is Aman Kharwal "+ Fore.YELLOW+ Back.BLUE+"I am your Machine Learning Instructor")
-print(Back.CYAN+"Hi My name is Aman Kharwal")
-print(Fore.RED + Back.GREEN+ "Hi My name is Aman Kharwal")
